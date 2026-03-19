83011bce2313c56c35e7ad9be62336161d7194ee
SikandarJODD <sikandar6174@gmail.com>
2026-03-19 20:13:23 +0530

    feat(testimonials): add testimonial components and integrate into the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,19 @@
 	import HeaderFive from '$lib/components/efferd/header/header-five/header-five.svelte';
 	import HeaderFour from '$lib/components/efferd/header/header-four/header-four.svelte';
 	import HeaderTwo from '$lib/components/efferd/header/header-two/header-two.svelte';
+	import TestimonialFour from '$lib/components/efferd/testimonial/testimonial-four/testimonial-four.svelte';
+	import TestimonialOne from '$lib/components/efferd/testimonial/testimonial-one.svelte';
+	import TestimonialThree from '$lib/components/efferd/testimonial/testimonial-three.svelte';
+	import TestimonialTwo from '$lib/components/efferd/testimonial/testimonial-two.svelte';
 	// import HeaderOne from '$lib/components/efferd/header/header/header-one.svelte';
 </script>
 
-<HeaderFive />
+<!-- <HeaderFive /> -->
 <!-- <div class="min-h-[300vh]">
 	<PageSkeletons />
 </div> -->
-<PageLoading />
+<!-- <PageLoading /> -->
+<!-- <TestimonialOne /> -->
+<div class="flex min-h-screen items-center justify-center">
+	<TestimonialFour />
+</div>
