aead76f35eccc7cf76d1ef7ddd5132d9f22c99b6
SikandarJODD <sikandar6174@gmail.com>
2026-03-21 03:47:22 +0530

    feat: added integrations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import IntegrationFive from '$lib/components/efferd/integrations/integration-five.svelte';
+	import IntegrationFour from '$lib/components/efferd/integrations/integration-four.svelte';
 	import IntegrationOne from '$lib/components/efferd/integrations/integration-one.svelte';
+	import IntegrationThree from '$lib/components/efferd/integrations/integration-three.svelte';
 	import IntegrationTwo from '$lib/components/efferd/integrations/integration-two.svelte';
 </script>
 
 <div class="space-y-10 py-10">
 	<IntegrationOne />
 	<IntegrationTwo />
+	<IntegrationThree />
+	<IntegrationFour />
+	<IntegrationFive />
 </div>
