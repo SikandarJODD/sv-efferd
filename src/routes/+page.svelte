8a21793c9de94250bbf2c3afc15c0d050bcba5d7
SikandarJODD <sikandar6174@gmail.com>
2026-03-20 12:39:29 +0530

    feat: added features section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,15 @@
 <script lang="ts">
-	import BlogOne from '$lib/components/efferd/blogs/blog-one/blog-one.svelte';
-	import BlogThree from '$lib/components/efferd/blogs/blog-three/blog-three.svelte';
-	import BlogTwo from '$lib/components/efferd/blogs/blog-two/blog-two.svelte';
-	import CtaFive from '$lib/components/efferd/cta/cta-five.svelte';
-	import CtaFour from '$lib/components/efferd/cta/cta-four.svelte';
-	import CtaOne from '$lib/components/efferd/cta/cta-one.svelte';
-	import CtaThree from '$lib/components/efferd/cta/cta-three.svelte';
-	import CtaTwo from '$lib/components/efferd/cta/cta-two.svelte';
-	import FaqFive from '$lib/components/efferd/faqs/faq-five.svelte';
-	import FaqFour from '$lib/components/efferd/faqs/faq-four.svelte';
-	import FaqOne from '$lib/components/efferd/faqs/faq-one.svelte';
-	import FaqThree from '$lib/components/efferd/faqs/faq-three.svelte';
-	import FaqTwo from '$lib/components/efferd/faqs/faq-two.svelte';
+	import FeatureOne from '$lib/components/efferd/features/feature-one.svelte';
+	import FeatureTwo from '$lib/components/efferd/features/feature-two.svelte';
+	import FeatureThree from '$lib/components/efferd/features/feature-three.svelte';
+	import FeatureFour from '$lib/components/efferd/features/feature-four/feature-four.svelte';
+	import FeatureFive from '$lib/components/efferd/features/feature-five/feature-five.svelte';
 </script>
 
 <div class="space-y-16 py-8">
-	<BlogOne />
-	<BlogTwo />
-	<BlogThree />
-
-	<div class="space-y-8 px-4 md:px-6">
-		<CtaOne />
-		<CtaTwo />
-		<CtaThree />
-		<CtaFour />
-		<CtaFive />
-	</div>
-
-	<FaqOne />
-	<FaqTwo />
-	<FaqThree />
-	<FaqFour />
-	<FaqFive />
+	<FeatureOne />
+	<FeatureTwo />
+	<FeatureThree />
+	<FeatureFour />
+	<FeatureFive />
 </div>
