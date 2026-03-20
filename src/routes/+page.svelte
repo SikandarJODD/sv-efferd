1c125a2328717247bd205095d60257eeed87adfb
SikandarJODD <sikandar6174@gmail.com>
2026-03-20 10:41:06 +0530

    feat: added first blog section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
-	import LogoCloudOne from '$lib/components/efferd/logo-cloud/logo-cloud-one.svelte';
-	import LogoCloudThree from '$lib/components/efferd/logo-cloud/three/logo-cloud-three.svelte';
-	import Showcase from '$lib/components/efferd/logo-cloud/three/showcase.svelte';
-	import LogoCloudTwo from '$lib/components/efferd/logo-cloud/two/logo-cloud-two.svelte';
-	// import Preview from '$lib/components/efferd/logo-cloud/two/preview.svelte';
-	import Preview from '$lib/components/efferd/logo-cloud/five/preview.svelte';
+	import BlogOne from '$lib/components/efferd/blogs/blog-one/blog-one.svelte';
 </script>
 
-<!-- <HeaderFive /> -->
-<!-- <div class="min-h-[300vh]">
-	<PageSkeletons />
-</div> -->
-<!-- <PageLoading /> -->
-<!-- <TestimonialOne /> -->
-<!-- <TestimonialFive /> -->
-<!-- <LogoCloudOne /> -->
-<!-- <LogoCloudTwo /> -->
-<!-- <Preview /> -->
-<Showcase />
-<Preview />
+<BlogOne />
