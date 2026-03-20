5f76ba30b5b2a51fd5e97342ccfaa3d8ca817e3f
SikandarJODD <sikandar6174@gmail.com>
2026-03-20 11:36:07 +0530

    feat: add BlogCard component and integrate into BlogThree section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import BlogOne from '$lib/components/efferd/blogs/blog-one/blog-one.svelte';
+	import BlogThree from '$lib/components/efferd/blogs/blog-three/blog-three.svelte';
 	import BlogTwo from '$lib/components/efferd/blogs/blog-two/blog-two.svelte';
 </script>
 
 <BlogOne />
 <BlogTwo />
+<BlogThree />
