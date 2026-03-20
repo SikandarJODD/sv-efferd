bdcf5aebc73c6a05b9ece44909deb002d54b8c56
SikandarJODD <sikandar6174@gmail.com>
2026-03-20 10:58:42 +0530

    feat: added blog section 2, aspect ratio component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import BlogOne from '$lib/components/efferd/blogs/blog-one/blog-one.svelte';
+	import BlogTwo from '$lib/components/efferd/blogs/blog-two/blog-two.svelte';
 </script>
 
 <BlogOne />
+<BlogTwo />
