7cf8131ff210a5060d7f45dc55c11222f4a322c9
SikandarJODD <sikandar6174@gmail.com>
2026-03-21 06:32:48 +0530

    feat: implement image gallery component with lazy loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-	import PricingOne from '$lib/components/efferd/pricing/pricing-one.svelte';
-	import PricingTwo from '$lib/components/efferd/pricing/pricing-two.svelte';
-	import PricingThree from '$lib/components/efferd/pricing/pricing-three.svelte';
-	import PricingFour from '$lib/components/efferd/pricing/pricing-four.svelte';
+	import ImageGallery from '$lib/components/efferd/image-gallery/image-gallery.svelte';
 </script>
 
-<main class="relative flex flex-col gap-24 px-4 py-10 md:py-16">
-	<PricingOne />
-	<PricingTwo />
-	<PricingThree />
-	<PricingFour />
-</main>
+<div class="p-10">
+	<ImageGallery />
+</div>
+
+<style>
+</style>
