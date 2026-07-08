e0aeb12722c7be57c38f65bd458471bb7ad17663
SikandarJODD <sikandar6174@gmail.com>
2026-07-09 04:01:47 +0530

    feat: improved hero page

--- src/lib/components/blocks/landing/blocks-hero.svelte
+++ src/lib/components/blocks/landing/blocks-hero.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { StripedPattern } from "$lib/components/magic/striped-pattern";
 	import { Button } from "$lib/components/ui/button";
-	import { DashedLines } from "$lib/components/ui/dashed-lines";
 	import { DecorIcon } from "$lib/components/ui/decor-icon";
 	import { Github } from "$lib/svgs";
+	import { draggable, bounds, BoundsFrom } from "@neodrag/svelte";
+	import { connectLine } from "$lib/utils/connect-line";
 </script>
 
 <section class="relative py-10 sm:py-12 lg:py-12">
@@ -31,15 +32,17 @@
 		position="bottom-right"
 	/>
 
-	<div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-		<div class="max-w-2xl text-left lg:max-w-3xl">
+	<div
+		class="relative mx-auto grid max-w-7xl grid-cols-1 px-4 sm:px-6 md:grid-cols-6 lg:px-8 lg:py-10"
+	>
+		<div class="max-w-2xl text-left md:col-span-4 lg:max-w-3xl">
 			<h1
-				class="max-w-2xl text-xl font-semibold tracking-tight text-foreground sm:text-3xl sm:leading-[1.08] lg:text-4xl lg:leading-[1.04]"
+				class="max-w-2xl text-3xl font-semibold tracking-tight text-foreground sm:text-3xl sm:leading-[1.08] lg:text-4xl lg:leading-[1.04]"
 			>
 				Svelte Marketing Blocks
 			</h1>
 
-			<p class="mt-3 max-w-2xl text-sm leading-6 text-muted-foreground sm:text-lg">
+			<p class="mt-3 max-w-2xl text-base leading-6 text-muted-foreground sm:text-lg">
 				<span class="font-mono">60+</span> UI & Marketing Blocks, built with Svelte, Tailwind
 				CSS and Shadcn.
 			</p>
@@ -95,6 +98,90 @@
 					Star on GitHub
 				</Button>
 			</div>
+		</div>
+		<div id="wrap" class="wrap relative hidden md:block md:col-span-2">
+			<div
+				id="product-pages"
+				class="absolute -top-5 -left-16 w-fit translate-z-10 rounded-full border-2 px-4 py-2 font-mono dark:border-emerald-400 dark:bg-emerald-900/10 dark:text-emerald-400"
+			>
+				Product Pages
+			</div>
+			<div
+				id="landing-pages"
+				class="absolute top-14 left-1/2 w-fit rounded-full border-2 px-4 py-2 font-mono dark:border-amber-400 dark:bg-amber-900/10 dark:text-amber-400"
+			>
+				Landing Pages
+			</div>
+			<div
+				id="open-source"
+				class="absolute top-28 left-4 w-fit rounded-full border-2 px-4 py-2 font-mono dark:border-blue-400 dark:bg-blue-900/10 dark:text-blue-400"
+			>
+				Open Source
+			</div>
+			<svg
+				style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none; overflow: visible;"
+			>
+				<path
+					use:connectLine={{
+						from: {
+							id: "product-pages",
+							position: "right",
+							offset: 20,
+							outset: 10
+						},
+						to: {
+							id: "landing-pages",
+							position: "left",
+							offset: 21.5,
+							outset: 45
+						},
+						container: "wrap",
+						radius: 50,
+						stroke: "white",
+						strokeWidth: 1.4
+					}}
+				/>
+				<path
+					use:connectLine={{
+						from: {
+							id: "landing-pages",
+							position: "bottom",
+							offset: 80,
+							outset: 0
+						},
+						to: {
+							id: "open-source",
+							position: "right",
+							offset: 20,
+							outset: 0
+						},
+						container: "wrap",
+						radius: 50,
+						stroke: "white",
+						strokeWidth: 1.4
+					}}
+				/>
+				<path
+					use:connectLine={{
+						from: {
+							id: "product-pages",
+							position: "bottom",
+							offset: 35,
+							outset: 0
+						},
+						to: {
+							id: "open-source",
+							position: "left",
+							offset: 20,
+							outset: 0
+						},
+						container: "wrap",
+						radius: 50,
+						stroke: "white",
+						strokeWidth: 1.4
+					}}
+				/>
+			</svg>
 		</div>
 	</div>
 </section>
